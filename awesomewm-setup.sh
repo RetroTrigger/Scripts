956009f0c5dc0c2c59593ad1563d1b2d9d0c1f29
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2020-08-14 12:11:42 +1000

    Update awesomewm-setup.sh

--- awesomewm-setup.sh
+++ awesomewm-setup.sh
@@ -6,13 +6,15 @@
 #sudo apt install awesome rofi picom lxappearance xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome kitty micro feh imagemagick bluez blueman xbacklight pcmanfm firefox xsecurelock -y
 
 #Arch-Based
-yay -S git awesome rofi picom gnome-polkit lxappearance flameshot pnmixer network-manager-applet xfce4-power-manager kitty micro feh imagemagick bluez blueman xbacklight pcmanfm firefox xsecurelock -y
+sudo pacman -Syyu -y
+sudo pacman -S yay -y
+yay -S git awesome rofi picom polkit-gnome lxappearance flameshot pnmixer network-manager-applet xfce4-power-manager kitty micro feh imagemagick bluez blueman xorg-xbacklight pcmanfm firefox xsecurelock -y
 
 #install Fonts
 git clone https://github.com/perrychan1/fonts.git
-sudo mv  -v ~/fonts/* /usr/share/fonts/
-rm -rf ~/fonts
-sudo apt install fonts-firacode
+sudo mv  -v ~/fonts /usr/share/fonts/
+sudo pacman -S ttf-fira-code -y
+#sudo apt install fonts-firacode
 fc-cache -f -v
 
 #Download Juno GTK Theme
@@ -21,8 +23,21 @@ git clone https://github.com/EliverLara/Juno.git
 sudo mv  -v ~/Juno /usr/share/themes/
 
 #Clone the configuration
-rm -rf ~/.config/awesome
-git clone https://github.com/RetroTrigger/awesome-config.git ~/.config
+cd ~
+git clone https://github.com/RetroTrigger/awesome-config.git 
+
+sudo rm -rf ~/.config/awesome
+sudo rm -rf ~/.config/rofi
+sudo rm -rf ~/.config/compton.conf
+
+sudo mv  -v ~/awesome-config/awesome ~/.config
+sudo mv  -v ~/awesome-config/images ~/.config
+sudo mv  -v ~/awesome-config/kitty ~/.config
+sudo mv  -v ~/awesome-config/rofi ~/.config
+sudo mv  -v ~/awesome-config/picom.conf ~/.config
+sudo mv  -v ~/awesome-config/tn0k20exrnb51.jpg ~/.config
+
+sudo rm -rf ~/awesome-config
 
 #Download Tela Green Icons
 cd ~
