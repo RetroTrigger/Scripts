812ea1d7845c582a53ef80fa583ceb947c25f8bb
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2020-08-14 11:30:49 +1000

    Update awesomewm-setup.sh

--- awesomewm-setup.sh
+++ awesomewm-setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #Debian-Based
-sudo add-apt-repository ppa:regolith-linux/unstable -y
-sudo apt update
-sudo apt install awesome rofi picom lxappearance xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome kitty micro feh imagemagick bluez blueman xbacklight pcmanfm firefox xsecurelock -y
+#sudo add-apt-repository ppa:regolith-linux/unstable -y
+#sudo apt update
+#sudo apt install awesome rofi picom lxappearance xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome kitty micro feh imagemagick bluez blueman xbacklight pcmanfm firefox xsecurelock -y
 
 #Arch-Based
-#yay -S awesome rofi picom i3lock-fancy xclip ttf-roboto gnome-polkit materia-gtk-theme lxappearance flameshot pnmixer network-manager-applet xfce4-power-manager -y
-#wget -qO- https://git.io/papirus-icon-theme-install | sh
+yay -S git awesome rofi picom gnome-polkit lxappearance flameshot pnmixer network-manager-applet xfce4-power-manager kitty micro feh imagemagick bluez blueman xbacklight pcmanfm firefox xsecurelock -y
 
 #install Fonts
 git clone https://github.com/perrychan1/fonts.git
