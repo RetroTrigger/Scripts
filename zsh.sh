66235793d9c0a73cdd55362668c710a31da0e02b
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2021-06-10 11:47:09 +1000

    Add files via upload

---
+++ zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm --needed  yay zsh zsh-syntax-highlighting zsh-autosuggestions
+yay -s --noconfirm --needed autojump
+
+wget https://github.com.ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
+mkdir -p "$HOME/.zsh"
+wget https://github.com/ChrisTitusTech/zsh/raw/master/.zsh/aliasrc -O ~/.zsh/aliasrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+chsh -s $(which zsh)
