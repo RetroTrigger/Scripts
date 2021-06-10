ebdc0a031066df813a5afda5e9b56da79044d38d
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2021-06-10 12:11:27 +1000

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,14 +2,19 @@
 
 #Dependencies
 sudo pacman -S --noconfirm --needed yay zsh zsh-syntax-highlighting zsh-autosuggestions
-yay -Sy --noconfirm --needed autojump ttf-meslo-nerd-font-powerlevel10k
+yay -Sy --noconfirm --needed autojump
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O /usr/share/fonts
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O /usr/share/fonts
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O /usr/share/fonts
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O /usr/share/fonts
+sudo fc-cache -f -v
 
 #Configs
 wget https://github.com.ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
 mkdir -p "$HOME/.zsh"
 wget https://github.com/ChrisTitusTech/zsh/raw/master/.zsh/aliasrc -O ~/.zsh/aliasrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 #Change Shell To ZSH
 chsh -s $(which zsh)
