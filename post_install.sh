49dc9e9c30a1e9fb9ede5a5cc8883786809fc97b
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2021-06-10 12:40:12 +1000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Enable Firewall
+sudo ufw enable
+sudo ufw status verbose
+sudo systemctl enable --now firewalld
+
+#Enable Trim
+sudo systemctl enable fstrim.timer
+
 ##graphical applications
 #pacman
 sudo pacman -S --noconfirm --needed \
@@ -14,40 +22,19 @@ sudo pacman -S --noconfirm --needed \
 	vlc \
 	terminator \
 	pfetch \
-	nitrogen \
-	variety \
-	rofi
+	variety
 
 #yay
 yay -S --noconfirm --needed \
 dropbox \
-shell-color-scripts \
-picom-tryone-git
+shell-color-scripts
 
 ##command line utilities
 sudo pacman -S --noconfirm --needed \
-	zsh \
-	yay \
-	feh \
 	bashtop \
 	git \
 	curl \
 	wget \
 	imagemagick \
 	lolcat \
-	base-devel \
-	w3m
-
-##fonts 
-sudo pacman -S --noconfirm --needed noto-fonts ttf-roboto
-yay -S --noconfirm --needed ttf-meslo-nerd-font-powerlevel10k ttf-ms-fonts ttf-vista-fonts
-fc-cache
-
-##oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-##change shell
-chsh -s $(which zsh)
+	base-devel
