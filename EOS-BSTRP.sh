ded34b33a5fa6f46c529f1ccac8aaa168728ecf3
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2021-12-26 12:15:50 +0000

    Update EOS-BSTRP.sh

--- EOS-BSTRP.sh
+++ EOS-BSTRP.sh
@@ -18,7 +18,6 @@ apt update && apt upgrade
 apt install git
 
 ##Uninstall Apps##
-apt purge epiphany-browser epiphany-browser-data
 apt purge pantheon-mail
 apt purge noise
 apt purge audience
@@ -55,13 +54,6 @@ cd la-capitaine-icon-theme && ./configure
 sudo add-apt-repository ppa:dyatlov-igor/la-capitaine
 apt install la-capitaine-cursor-theme
 
-##Proper Plank##
-sudo add-apt-repository ppa:ricotz/docky
-apt update
-apt install --reinstall plank
-killall plank
-plank --preferences
-
 ## APPS TO DOWNLOAD & INSTALL
 
 ##Brave Browser##
@@ -75,13 +67,6 @@ apt update
 
 apt install brave-browser
 
-##Sublime Text 3##
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install sublime-text
-
 ##Wine
 sudo dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
@@ -89,10 +74,6 @@ sudo apt-key add winehq.key
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 apt update
 apt install --install-recommends winehq-staging
-
-##Java
-apt update
-apt install openjdk-8-jdk openjdk-8-jre
 
 ## APPS TO INSTALL & CONFIGURE VIA TERMINAL
 
@@ -107,20 +88,6 @@ sudo dpkg-reconfigure libdvd-pkg
 
 ##archive formats##
 apt install unace rar unrar p7zip-rar p7zip sharutils uudeview mpack arj cabextract lzip lunzip
-
-## Other Tweaks/Fixes
-
-##ZSH##
-apt install zsh zsh-syntax-highlighting autojump zsh-autosuggestions
-mkdir ~/zsh
-wget -L https://raw.githubusercontent.com/ChrisTitusTech/zsh/master/aliasrc ~/zsh/aliasrc
-wget -L https://raw.githubusercontent.com/ChrisTitusTech/zsh/master/.zshrc
-touch "$HOME/.cache/zshhistory"
-#-- Setup Alias in $HOME/zsh/aliasrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-chsh -s $(which zsh)
-echo "alias apt='apt-fast'" >> ~/zsh/aliasrc
 
 ##Reduce Overheating & Improve Battery Life##
 sudo add-apt-repository ppa:linrunner/tlp
