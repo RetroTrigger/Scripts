196fcaac6799ae018deaace47bfeb2f5b9627552
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2024-09-12 14:51:36 +1000

    Updated Readme

--- README.md
+++ README.md
@@ -39,3 +39,8 @@ Use the following command to run the script easily in one run:
 ```bash
 wget -O - https://raw.githubusercontent.com/RetroTrigger/.Scripts/master/Ultimate-Linux-Gaming.sh | bash
 ```
+Proxmox VM Coversion And Import Script
+Use the following command to run the script easily in one run:
+```bash
+wget -O - https://raw.githubusercontent.com/RetroTrigger/.Scripts/master/vm-import-prox.sh | bash
+```
---
+++ vm-import-prox.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# Variables
+TEMPLATE_DIR="/var/lib/vz/template/imported_templates"
+NETWORK_SHARE="/mnt/templates"
+STORAGE="local-lvm"  # Proxmox storage to use
+
+# Create and share the folder
+mkdir -p $TEMPLATE_DIR
+chmod 775 $TEMPLATE_DIR
+
+# Share the folder via NFS (Example)
+echo "$TEMPLATE_DIR *(rw,sync,no_root_squash)" >> /etc/exports
+exportfs -a
+systemctl restart nfs-kernel-server
+
+# Function to convert OVA/VMDK files and create a VM
+convert_and_create_vm() {
+    VMID=$1
+    VM_NAME=$2
+    TEMPLATE_FILE="$3"
+    STORAGE_TYPE=$4
+
+    echo "Converting and creating VM with ID $VMID and name $VM_NAME..."
+
+    # Check if the file is OVA or VMDK and convert accordingly
+    if [[ $TEMPLATE_FILE == *.ova ]]; then
+        echo "Converting OVA file..."
+        qm importovf $VMID $TEMPLATE_FILE $STORAGE --format qcow2
+    elif [[ $TEMPLATE_FILE == *.vmdk ]]; then
+        echo "Converting VMDK file..."
+        qm importdisk $VMID $TEMPLATE_FILE $STORAGE --format qcow2
+        qm set $VMID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-$VMID-disk-0
+    else
+        echo "Unsupported file type: $TEMPLATE_FILE"
+        return 1
+    fi
+
+    # Configure the VM
+    qm create $VMID --name $VM_NAME --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+    qm set $VMID --boot c --bootdisk scsi0 --serial0 socket --vga serial0
+
+    # Start the VM
+    qm start $VMID
+    echo "VM $VM_NAME with ID $VMID has been created and started."
+}
+
+# Function to download VulnHub templates (optional)
+download_vuln() {
+    read -p "Enter the download link: " LINK
+    echo "Downloading template from $LINK..."
+    wget $LINK -P $TEMPLATE_DIR
+    tar -xvf $TEMPLATE_DIR/$(basename $LINK) -C $TEMPLATE_DIR
+    echo "Template downloaded and extracted to $TEMPLATE_DIR."
+}
+
+# Function to manage VMs
+manage_vms() {
+    echo "1) Start VM"
+    echo "2) Stop VM"
+    echo "3) Destroy VM"
+    read -p "Choose an action: " ACTION
+    read -p "Enter the VM ID: " VMID
+
+    case $ACTION in
+        1)
+            qm start $VMID
+            echo "VM $VMID started."
+            ;;
+        2)
+            qm stop $VMID
+            echo "VM $VMID stopped."
+            ;;
+        3)
+            qm destroy $VMID
+            echo "VM $VMID destroyed."
+            ;;
+        *)
+            echo "Invalid action."
+            ;;
+    esac
+}
+
+# Main menu
+while true; do
+    echo "1) Convert and create VM"
+    echo "2) Download VulnHub template"
+    echo "3) Manage VMs"
+    echo "x) Exit"
+    read -p "Choose an option: " OPTION
+
+    case $OPTION in
+        1)
+            read -p "Enter VM ID: " VMID
+            read -p "Enter VM name: " VM_NAME
+            read -p "Enter path to template file: " TEMPLATE_FILE
+            read -p "Enter storage type (1 for LVM-Thin, 2 for Directory): " STORAGE_TYPE
+            convert_and_create_vm $VMID $VM_NAME $TEMPLATE_FILE $STORAGE_TYPE
+            ;;
+        2)
+            download_vuln
+            ;;
+        3)
+            manage_vms
+            ;;
+        x)
+            echo "Exiting..."
+            break
+            ;;
+        *)
+            echo "Invalid option."
+            ;;
+    esac
+done
