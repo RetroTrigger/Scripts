91f7ff935af6fdff1db614bf3d4eb053dbad8c5e
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2025-12-17 07:36:37 +1000

    Update dwm-install.sh

--- dwm-install.sh
+++ dwm-install.sh
@@ -1,57 +1,108 @@
 #!/bin/bash
 
-install_packages() {
-    echo "Updating system and installing packages..."
-    sudo pacman -Syyu --noconfirm
-    
-    # Install official repository packages
-    echo "Installing official repository packages..."
-    sudo pacman -S --noconfirm \
-        nitrogen \
-        steam \
-        xorg-server xorg-xinit xorg-xrandr xorg-xsetroot \
-        git \
-        base-devel \
-        feh \
-        lxappearance \
-        polybar \
-        thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin \
-        gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc gvfs-nfs gvfs-smb \
-        mate-polkit \
-        picom \
-        flameshot \
-        imagemagick \
-        ttf-dejavu ttf-liberation noto-fonts ttf-droid ttf-iosevka-nerd  # Basic and additional fonts
-    
-    # Install yay if not already installed
-    if ! command -v yay &> /dev/null; then
-        echo "Installing yay..."
-        git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
-        (cd /tmp/yay-bin && makepkg -si --noconfirm)
+# Global variables for package manager and commands
+PKG_MANAGER=""
+UPDATE_CMD=""
+INSTALL_CMD=""
+
+detect_package_manager() {
+    if command -v pacman &> /dev/null; then
+        PKG_MANAGER="pacman"
+        UPDATE_CMD="sudo pacman -Syyu --noconfirm"
+        INSTALL_CMD="sudo pacman -S --noconfirm"
+    elif command -v apt-get &> /dev/null; then
+        PKG_MANAGER="apt"
+        UPDATE_CMD="sudo apt-get update"
+        INSTALL_CMD="sudo apt-get install -y"
+    elif command -v dnf &> /dev/null; then
+        PKG_MANAGER="dnf"
+        UPDATE_CMD="sudo dnf check-update"
+        INSTALL_CMD="sudo dnf install -y"
+    else
+        echo "Unsupported package manager. This script supports pacman, apt, and dnf." >&2
+        exit 1
     fi
-    
-    # Install AUR packages
-    echo "Installing AUR packages..."
-    yay -S --noconfirm \
-        brave-bin \
-        ttf-meslo-nerd-font-powerlevel10k
+    echo "Package manager detected: $PKG_MANAGER"
+}
+
+install_packages() {
+    echo "Updating system..."
+    eval $UPDATE_CMD
+
+    echo "Installing packages..."
+    local packages
+    local build_essentials
+
+    case "$PKG_MANAGER" in
+        "pacman")
+            packages="nitrogen steam xorg-server xorg-xinit xorg-xrandr xorg-xsetroot git feh lxappearance polybar thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc gvfs-nfs gvfs-smb mate-polkit picom flameshot imagemagick ttf-dejavu ttf-liberation noto-fonts ttf-droid ttf-iosevka-nerd"
+            build_essentials="base-devel"
+            ;;
+        "apt")
+            packages="nitrogen steam xserver-xorg xinit x11-xserver-utils git feh lxappearance polybar thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs gvfs-backends gvfs-fuse policykit-1-gnome picom flameshot imagemagick fonts-dejavu fonts-liberation fonts-noto fonts-droid-fallback fonts-nerd-font-mono"
+            build_essentials="build-essential"
+            ;;
+        "dnf")
+            packages="nitrogen steam xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xrandr git feh lxappearance polybar thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc gvfs-nfs gvfs-smb polkit-gnome picom flameshot ImageMagick dejavu-sans-fonts liberation-fonts google-noto-sans-fonts droid-sans-fonts"
+            build_essentials="@development-tools"
+            ;;
+    esac
+
+    eval $INSTALL_CMD $build_essentials $packages
+
+    # Install third-party packages
+    install_third_party_packages
+}
+
+install_third_party_packages() {
+    echo "Installing third-party packages..."
+    case "$PKG_MANAGER" in
+        "pacman")
+            if ! command -v yay &> /dev/null; then
+                echo "Installing yay..."
+                git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+                (cd /tmp/yay-bin && makepkg -si --noconfirm)
+            fi
+            yay -S --noconfirm brave-bin ttf-meslo-nerd-font-powerlevel10k
+            ;;
+        "apt")
+            # Brave Browser
+            sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+            eval $UPDATE_CMD
+            eval $INSTALL_CMD brave-browser
+            # Nerd Fonts
+            # apt does not have a direct equivalent for ttf-meslo-nerd-font-powerlevel10k, manual install would be needed.
+            echo "Please install Meslo Nerd Font manually for Debian/Ubuntu systems."
+            ;;
+        "dnf")
+            # Brave Browser
+            sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+            sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+            eval $INSTALL_CMD brave-browser
+            # Nerd Fonts
+            echo "Please install Meslo Nerd Font manually for Fedora systems."
+            ;;
+    esac
 }
 
 clone_repositories() {
     echo "Cloning repositories..."
-    mkdir -p ~/.config
-    
+    local suckless_dir="$HOME/.config/suckless"
+    mkdir -p "$suckless_dir"
+
     # Clone repositories if they don't exist
-    [ ! -d "$HOME/.config/dwm" ] && git clone https://github.com/bakkeby/dwm-flexipatch.git ~/.config/dwm
-    [ ! -d "$HOME/.config/st" ] && git clone https://github.com/bakkeby/st-flexipatch.git ~/.config/st
-    [ ! -d "$HOME/.config/dmenu" ] && git clone https://github.com/bakkeby/dmenu-flexipatch.git ~/.config/dmenu
+    [ ! -d "$suckless_dir/dwm" ] && git clone https://github.com/bakkeby/dwm-flexipatch.git "$suckless_dir/dwm"
+    [ ! -d "$suckless_dir/st" ] && git clone https://github.com/bakkeby/st-flexipatch.git "$suckless_dir/st"
+    [ ! -d "$suckless_dir/dmenu" ] && git clone https://github.com/bakkeby/dmenu-flexipatch.git "$suckless_dir/dmenu"
 }
 
 compile_software() {
     echo "Compiling software..."
-    [ -d "$HOME/.config/dwm" ] && (cd ~/.config/dwm && sudo make clean install)
-    [ -d "$HOME/.config/st" ] && (cd ~/.config/st && sudo make clean install)
-    [ -d "$HOME/.config/dmenu" ] && (cd ~/.config/dmenu && sudo make clean install)
+    local suckless_dir="$HOME/.config/suckless"
+    [ -d "$suckless_dir/dwm" ] && (cd "$suckless_dir/dwm" && sudo make clean install)
+    [ -d "$suckless_dir/st" ] && (cd "$suckless_dir/st" && sudo make clean install)
+    [ -d "$suckless_dir/dmenu" ] && (cd "$suckless_dir/dmenu" && sudo make clean install)
 }
 
 setup_xinitrc() {
@@ -62,10 +113,7 @@ setup_xinitrc() {
         echo "#!/bin/sh\n\n# Start DWM with mate-polkit\nexec mate-polkit &\nexec dwm" > ~/.xinitrc
         chmod +x ~/.xinitrc
     else
-        echo "~/.xinitrc already exists. Please add the following lines to start dwm with mate-polkit:"
-        echo "\n# Start DWM with mate-polkit"
-        echo "exec mate-polkit &"
-        echo "exec dwm"
+        echo "~/.xinitrc already exists. Please ensure it is configured to start dwm."
     fi
 }
 
@@ -73,38 +121,67 @@ setup_autostart() {
     echo "Setting up X and DWM autostart..."
     local autostart_cmd='if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then exec startx; fi'
     
-    # Check which shell profile files exist
     local shell_rc=""
-    if [ -f "$HOME/.bash_profile" ]; then
-        shell_rc="$HOME/.bash_profile"
-    elif [ -f "$HOME/.bashrc" ]; then
-        shell_rc="$HOME/.bashrc"
-    else
-        shell_rc="$HOME/.bash_profile"
-        touch "$shell_rc"
-    fi
-    
-    # Check if autostart is already set up
+    [ -f "$HOME/.bash_profile" ] && shell_rc="$HOME/.bash_profile" || shell_rc="$HOME/.bashrc"
+    [ ! -f "$shell_rc" ] && touch "$HOME/.bash_profile" && shell_rc="$HOME/.bash_profile"
+
     if grep -q "exec startx" "$shell_rc"; then
         echo "Autostart for X is already configured in $shell_rc"
     else
         echo "Adding X autostart to $shell_rc"
         echo -e "\n# Auto-start X on tty1 login" >> "$shell_rc"
         echo "$autostart_cmd" >> "$shell_rc"
-        echo "Autostart configuration added to $shell_rc"
     fi
 }
 
+create_dwm_desktop_entry() {
+    echo "Creating DWM desktop entry..."
+    local desktop_entry="[Desktop Entry]\nEncoding=UTF-8\nName=DWM\nComment=Dynamic Window Manager\nExec=dwm\nIcon=\nType=XSession"
+    echo -e "$desktop_entry" | sudo tee /usr/share/xsessions/dwm.desktop > /dev/null
+}
+
+setup_display() {
+    echo "Select how you want to start DWM:"
+    echo "1) Use a Display Manager (e.g., LightDM, GDM, SDDM) - Recommended"
+    echo "2) Use startx (manual start from tty)"
+    read -p "Enter your choice [1-2]: " choice
+
+    case $choice in
+        1) # Display Manager
+            create_dwm_desktop_entry
+            local dm_installed=$(systemctl list-units --type=service | grep -E 'lightdm|gdm|sddm')
+            if [ -n "$dm_installed" ]; then
+                echo "Found an existing display manager. DWM will be available as a session."
+            else
+                echo "No display manager found."
+                read -p "Would you like to install one? (lightdm) [y/N]: " install_dm
+                if [[ "$install_dm" =~ ^[yY](es)?$ ]]; then
+                    sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter
+                    sudo systemctl enable lightdm.service
+                    echo "LightDM has been installed and enabled. Please reboot after installation."
+                fi
+            fi
+            echo -e "\nInstallation complete! Reboot and select DWM from your display manager's session list."
+            ;;
+        2) # startx
+            setup_xinitrc
+            setup_autostart
+            echo -e "\nInstallation complete!"
+            echo -e "\nTo start DWM, log in to tty1 and X will start automatically, or type 'startx'."
+            ;;
+        *)
+            echo "Invalid choice. Exiting."
+            exit 1
+            ;;
+    esac
+}
+
 main() {
+    detect_package_manager
     install_packages
     clone_repositories
     compile_software
-    setup_xinitrc
-    setup_autostart
-    
-    echo -e "\nInstallation complete!"
-    echo -e "\nTo start DWM, simply log in to tty1 and X will start automatically."
-    echo -e "If you need to start DWM manually, type 'startx' after logging in."
+    setup_display
 }
 
 # Run the main function
